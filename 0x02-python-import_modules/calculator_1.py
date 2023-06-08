ef23376b47f4767bbeb8fdf5e0c4dd8bed69b89d
Johntista85 <johntista85@gmail.com>
2023-06-08 04:22:32 -0700

    import_modules tasks

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+def add(c, b):
+    """My addition function
+
+    Args:
+        c: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (c + b)
+
+def sub(c, b):
+    """My subtraction function
+
+    Args:
+        c: first integer
+        b: second integer
+
+    Returns:
+        The return value. c - b
+    """
+    return (c - b)
+
+def mul(c, b):
+    """My multiplication function
+
+    Args:
+        c: first integer
+        b: second integer
+
+    Returns:
+        The return value. c * b
+    """
+    return (c * b)
+
+def div(c, b):
+    """My division function
+
+    Args:
+        c: first integer
+        b: second integer
+
+    Returns:
+        The return value. c / b
+    """
+    return int(c / b)
