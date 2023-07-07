d5e8dd94a90f6d667fcd58ad763bb900548eeb4c
Johntista85 <johntista85@gmail.com>
2023-07-07 02:03:07 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,3 @@
-
 /*
  * File: 102-python.c
  */
@@ -18,16 +17,16 @@ void print_python_string(PyObject *p)
 	printf("[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		printf("  [ERROR] Invalid String Object \n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
 	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii \n");
+		printf("  type: compact ascii\n");
 	else
-		printf("  type: compact unicode object \n");
+		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
 	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
